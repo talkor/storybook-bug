8122412e4d77ce45fe477f0ad9367547dfef84bd
talkor <talkor@users.noreply.github.com>
2020-11-22 10:46:45 +0200

    initial commit

---
+++ src/stories/Button.js
@@ -0,0 +1,68 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./button.css";
+
+const Type = {
+  PRIMARY: "primary",
+  SECONDARY: "secondary",
+};
+
+const { PRIMARY, SECONDARY } = Type;
+/**
+ * Primary UI component for user interaction
+ */
+export const Button = ({
+  primary,
+  backgroundColor,
+  size,
+  label,
+  type,
+  ...props
+}) => {
+  const mode = primary
+    ? "storybook-button--primary"
+    : "storybook-button--secondary";
+  return (
+    <button
+      type="button"
+      className={["storybook-button", `storybook-button--${size}`, mode].join(
+        " "
+      )}
+      style={backgroundColor && { backgroundColor }}
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  type: PropTypes.oneOf([PRIMARY, SECONDARY]),
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary: PropTypes.bool,
+  /**
+   * What background color to use
+   */
+  backgroundColor: PropTypes.string,
+  /**
+   * How large should the button be?
+   */
+  size: PropTypes.oneOf(["small", "medium", "large"]),
+  /**
+   * Button contents
+   */
+  label: PropTypes.string.isRequired,
+  /**
+   * Optional click handler
+   */
+  onClick: PropTypes.func,
+};
+
+Button.defaultProps = {
+  backgroundColor: null,
+  primary: false,
+  size: "medium",
+  onClick: undefined,
+};
